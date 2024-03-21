ff2eaf5f9e115a2579ddd86fc45ba45ba2faf651
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2024-03-21 12:59:59 +0530

    Footer done.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import { ImPower } from "react-icons/im";
 import { flushSync } from "react-dom";
 import { SiFerrari } from "react-icons/si";
 import { CiDollar } from "react-icons/ci";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdOutlineMail } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
+
 
 // https://react-icons.github.io/react-icons/search/#q=power
 
@@ -16,12 +20,13 @@ const App = () => {
 
   return (
     <>
-      <div className="fixed top-0 w-full h-14 bg-[#0A0A0A] flex justify-between items-center pl-6 pr-6 z-10"> 
-        <SiFerrari size={30} color="white"/>
+    {/* <div className="flex flex-col"> */}
+      <div className="fixed top-0 w-full h-16 bg-[#0A0A0A] flex justify-between items-center pl-6 pr-6 z-10"> 
+        <SiFerrari size={35} color="white"/>
       <div className="flex gap-12">
         <div className="text-white hover:cursor-pointer hover:underline">Home</div>
         <div className="text-white" >Collection</div>
-        <div className="text-white" >Reviews</div>
+        {/* <div className="text-white" >Reviews</div> */}
         <div className="text-white" >About Us</div>
       </div>
       <div className="text-white">
@@ -56,16 +61,6 @@ const App = () => {
         </div>
       </div>
 
-
-      {/* <div className="bg-gray-50 grid sm:grid-cols-2 md:grid-cols-3 gap-4 p-4 md:px-14">
-        <CarCard audi={true}/>
-        <CarCard2/>
-        <CarCard2/>
-        <CarCard/>
-        <CarCard/>
-        <CarCard/>
-        <CarCard/>
-      </div> */}
       <div className="bg-white h-[100vh]">
         <div className="bg-black relative">
           <img className="w-full" src="/supercar-lineup.jpg" alt="" />
@@ -77,12 +72,12 @@ const App = () => {
           </div>
         </div>
         
-        <div className="grid md:grid-cols-3 gap-10 px-14 my-7 items-center">
+        <div className="grid md:grid-cols-3 gap-3 lg:gap-10 px-5 lg:px-[100px] my-5 lg:my-7 items-center ">
           <div className="bg-gray-100 border rounded-3xl flex justify-between px-4">
             <CiDollar size={150}/>
-            <div className="flex flex-col justify-around m-4">
+            <div className="flex flex-col justify-evenly m-4">
               <div className="text-xl font-bold">All Inclusive Pricing</div>
-              <div>Get everything you need in one convenient, transparent price with our all-inclusive pricing policy.</div>
+              <div>Get everything you need in one convenient, transparent price.</div>
             </div>
           </div>
           <div className="bg-gray-100 border  rounded-3xl flex justify-between px-4">
@@ -103,32 +98,56 @@ const App = () => {
 
 
       </div>
+        <div className="bg-gray-200 grid sm:grid-cols-2 lg:grid-cols-10 p-10 lg:p-20 gap-10">
+          
+          {/* skyzone dealership */}
+          <div className="flex flex-col text-black gap-6 text-md col-span-3">
+            <div className="text-3xl font-black">
+              Skyzone Dealership
+            </div>
+            <div>
+              We offers a big range of vehicles for all your driving needs. 
+              We have the perfect car to meet your needs.
+            </div>
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2"> <FaPhoneAlt /> +91 (768)30 89932</div>
+              <div className="flex items-center gap-2"> <MdOutlineMail /> dealers@skyzone.com</div>
+              <div className="flex items-center gap-2"> <FaLocationDot />65/13, Dwarka, IN</div>
+            </div>
+          </div>
 
-      
+          {/* company */}
+          <div className="flex flex-col text-black gap-5 text-md col-span-2">
+            <div className="text-3xl font-bold">
+              Company
+            </div>
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2"> Careers</div>
+              <div className="flex items-center gap-2"> Blogs</div>
+              <div className="flex items-center gap-2">Blog</div>
+            </div>
+          </div>
 
+          {/* working hours */}
+          <div className="flex flex-col text-black gap-5 text-md col-span-2">
+            <div className="text-3xl font-bold">Working Hours</div>
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-2">Mon - Fri: 9:00AM - 9:00PM</div>
+              <div className="flex items-center gap-2">Sat: 9:00AM - 19:00PM</div>
+              <div className="flex items-center gap-2">Sun: Closed</div>
+            </div>
+          </div>
 
-    <div className="bg-gray-50">
-      <div className="text-center mb-10">
-        <div className="text-black font-semibold text-2xl mb-2">Supercar Models</div>
-        <div className="text-black font-bold text-4xl mb-2">Explore Our Exotic collection</div>
-        <div className="text-gray-500 ">Indulge in the ultimate driving experience with our exquisite selection of supercars</div>
+          {/* subscribe */}
+          <div className="flex flex-col text-black gap-5 text-md col-span-3">
+            <div className="text-3xl font-bold">Subscription</div>
+            <div className="flex flex-col gap-3">
+              <div className="flex items-center">Subscribe your Email address for latest news & updates.</div>
+              <input type="email" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-black focus:border-black block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required />
+              <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Subscribe</button>
+            </div>
+          </div>
       </div>
-      <div className="grid md:grid-cols-3 sm:grid-col-1 gap-10 bg-gray-50 h-96">
-        <div className="flex flex-col justify-center items-center gap-2">
-          <CarTab name="LaFerrari"/>
-          <CarTab name="Mclaren 720s"/>
-          <CarTab name="Ferrari Roma"/>
-          <CarTab name="Porsche 911"/>
-          <CarTab name="lamborghini Revuelto"/>
-          <CarTab name="Bugatti Chiron"/>
-          <CarTab name="Aston Martin Valhalla"/>
-        </div>
-        <img className="flex justify-center" src="/laferrari-jaguar-cars-enzo-ferrari-ferrari-removebg-preview.png" alt="car" />
-        <div className="bg-yellow-500">
-
-        </div>
-      </div>
-    </div>
     </>
   )
 }
---
+++ src/collection.jsx
@@ -0,0 +1,11 @@
+const CollectionPage = () => {
+    <div className="bg-gray-50 grid sm:grid-cols-2 md:grid-cols-3 gap-4 p-4 md:px-14">
+        <CarCard audi={true}/>
+        <CarCard2/>
+        <CarCard2/>
+        <CarCard/>
+        <CarCard/>
+        <CarCard/>
+        <CarCard/>
+      </div>
+}
